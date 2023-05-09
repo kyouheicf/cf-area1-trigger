17043c30e0f7ede4e730bb55d4253b0f0e548a71
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2023-05-10 01:04:38 +0900

    initial commit

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,14 +1,14 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
 export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
-    personalizations: ({ formData, }) => {
-        return [
-            {
-                to: [{ name: "ACME Support", email: `${context.env.RECEIVER_EMAIL}`, }],
-                headers: { "X-Area1Security-Request-Disposition": formData.get('disposition').toString() },
+    personalizations: [
+        {
+            to: [{ name: "ACME Support", email: `${context.env.RECEIVER_EMAIL}`, }],
+            headers: {
+                "X-Area1Security-Request-Disposition": `${context.request.formData().get('disposition').toString()}`
             },
-        ];
-    },
+        },
+    ],
     from: { name: "Contact Us Form Inquiry", email: `${context.env.SENDER_EMAIL}` },
     respondWith: () => {
         return new Response(
